4d8f4d7eea3dd7eb7e3a99f9acfc43cfc651ecba
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-10-25 16:40:38 +0300

    Factory_Method

---
+++ Pseudocode_Factory_Method.cs
@@ -0,0 +1,77 @@
+﻿// Класът създател декларира Фактори методa, който трябва
+// връща обект от продуктов клас. Подкласовете на създателя
+// обикновено предоставя имплементацията на този метод.
+class Dialog is
+    // Създателят може също така да предостави някаква реализация по подразбиране
+    // на Фактори методa.
+    abstract method createButton():Button
+
+    // Имайте предвид, че въпреки името си, основният създател на
+     // отговорността не е създаването на продукти. Обикновено
+     // съдържа някаква основна бизнес логика, която разчита на продукта
+     // обекти, върнати от Фактори метода. Подкласовете могат
+     // косвено да променят тази бизнес логика, като отменя
+     // Фактори методa и връщане на различен тип продукт
+     // от него.
+    method render() is
+        // Извикване на Фактори метода за създаване на продуктов обект.
+        Button okButton = createButton()
+        // Сега използвайки продукта.
+        okButton.onClick(closeDialog)
+        okButton.render()
+
+
+// Конкретните създатели заменят Фактори метода, за да променят
+// типът на получения продукт.
+class WindowsDialog extends Dialog is
+    method createButton():Button is
+        return new WindowsButton()
+
+class WebDialog extends Dialog is
+    method createButton():Button is
+        return new HTMLButton()
+
+
+// Интерфейсът на продукта декларира операциите, които всички
+// конкретни продукти трябва да изпълняват.
+interface Button is
+    method render()
+    method onClick(f)
+
+// Конкретните продукти осигуряват различни изпълнения на
+// продуктовия интерфейс.
+class WindowsButton implements Button is
+    method render(a, b) is
+        // Render a button in Windows style.
+    method onClick(f) is
+        // Bind a native OS click event.
+
+class HTMLButton implements Button is
+    method render(a, b) is
+        // Return an HTML representation of a button.
+    method onClick(f) is
+        // Bind a web browser click event.
+
+
+class Application is
+    field dialog: Dialog
+
+    // Приложението избира типа създател в зависимост от
+    // текуща конфигурация или настройки на средата.
+    method initialize() is
+        config = readApplicationConfigFile()
+
+        if (config.OS == "Windows") then
+            dialog = new WindowsDialog()
+        else if (config.OS == "Web") then
+            dialog = new WebDialog()
+        else
+            throw new Exception("Error! Unknown operating system.")
+
+    // Клиентският код работи с инстанция на конкретния
+     // създател, макар и през неговия базов интерфейс. Докато
+     // клиентът продължава да работи със създателя чрез базата
+     // интерфейс, можете да му предадете подклас на всеки създател.
+    method main() is
+        this.initialize()
+        dialog.render()
---
+++ RealWorld_Factory_Method.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+/*
+ Използвайки градивния шаблон за дизайн Factory Method създайте програма на C#, която
+да създава WindowsDialog или WebDialog, следователно по даден environment type(тип среда).
+Всеки 1 диалог може да създава съответно WindowsDialog -> WindowsButton & WebDialog -> HTMLButton.
+Ако средата не е нито windows или web трябва да хвърлите грешка. Всеки тип бутон трябва да има
+функционалност за рендериране и при клик да прави нещо (да изписва нещо на конзолата). В Main
+метода създайте 3 диалога (1 windows, 1 web и 1 невалиден), създайте съответните бутони, рендерирайте ги
+и изпълнете при клик метода.
+ */
+namespace FactoryMethod
+{
+   //абстрактния клас ще бъде наследен от конкретните Диалози
+    abstract class Dialog
+    {
+       
+        public abstract Button createButton();
+
+        public string render()
+        {
+          
+            Button button = createButton();
+       
+            string result = string.Format("{0} е създаден.",button.GetType().Name);
+            
+            return result;
+        }
+    }
+
+    //Конкретен клас 1
+    class WindowsDialog : Dialog
+    {
+      
+        public override Button createButton()
+        {
+            return new WindowsButton();
+        }
+    }
+
+    //Конкретен клас 2
+    class WebDialog : Dialog
+    {
+        public override Button createButton()
+        {
+            return new HTMLButton();
+        }
+    }
+
+    //интерфейс за самите Бутони,който ще биват създавани постредством Диалозите
+    public interface Button
+    {
+        string render();
+        string onClick(string target);
+    }
+
+    //Конкретен бутон 1
+    class WindowsButton : Button
+    {
+ 
+
+        public string onClick(string target)
+        {
+            return string.Format("{0} е кликнат", target);
+        }
+
+        public string render()
+        {
+            return string.Format("{0} е показан(дисплейнат)", this.GetType().Name);
+        }
+    }
+
+    //Конкретен бутон 2
+    class HTMLButton : Button
+    {
+        public string onClick(string target)
+        {
+            return string.Format("{0} е кликнат", target);
+        }
+
+        public string render()
+        {
+            return string.Format("{0} е показан(дисплейнат)", this.GetType().Name);
+        }
+    }
+
+
+    class Program
+    {
+        //Самият Factory Method, който решава какъв Диалог да се създаде
+        //Посредством подаден environmentType
+        public static Dialog initializer(string environmentType)
+        {
+            Dialog dialog;
+            if (environmentType == "Windows")
+            {
+                dialog = new WindowsDialog();
+            }
+            else if (environmentType == "Web")
+            {
+                dialog = new WebDialog();
+            }
+            else throw new InvalidOperationException("Грешка! Непозната среда!");
+
+            return dialog;
+        }
+        static void Main(string[] args)
+        {
+            Dialog dialogWin = initializer("Windows");
+            Console.WriteLine(dialogWin.render()); 
+            Button winButton = dialogWin.createButton();
+            Console.WriteLine(winButton.render());
+            Console.WriteLine(winButton.onClick("Exit"));
+
+            Console.WriteLine();
+            
+            Dialog dialogWeb = initializer("Web");
+            Console.WriteLine(dialogWeb.render());
+            Button webButton = dialogWeb.createButton();
+            Console.WriteLine(webButton.render());
+            Console.WriteLine(webButton.onClick("Log in"));
+
+            Console.WriteLine();
+
+            Dialog dialogError = initializer("Linux");
+        }
+    }
+}
